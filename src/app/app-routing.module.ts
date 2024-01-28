4c2ab144b0976d17659a9acc55447846e60829bf
koskrit <koskritm@gmail.com>
2024-01-28 04:18:40 +0200

    Create Footer Section & Import all Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { ViewComponent } from './pages/view/view.component';
+import { NotFound404Component } from './pages/not-found404/not-found404.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: 'view',
+    component: ViewComponent,
+  },
+  {
+    path: '**',
+    component: NotFound404Component,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { HomeComponent } from './pages/home/home.component';
+import { CreateComponent } from './pages/create/create.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { ViewComponent } from './pages/view/view.component';
+import { ArticleComponent } from './pages/view/components/article/article.component';
+import { NotFound404Component } from './pages/not-found404/not-found404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    CreateComponent,
+    FooterComponent,
+    ViewComponent,
+    ArticleComponent,
+    NotFound404Component,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

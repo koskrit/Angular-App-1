862bca0e6d73c6680266d7f3f1125d2a82da93de
koskrit <koskritm@gmail.com>
2024-01-28 04:11:25 +0200

    Setup Header Mobile Menu & User Icon display

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  signedIn = true;
+  showModal = false;
+  showMobileMenu = false;
 
+  constructor(private elementRef: ElementRef) {}
+
+  getSignedInfo() {
+    return this.signedIn;
+  }
+
+  toggleModal() {
+    this.showModal = !this.showModal;
+  }
+
+  toggleMobileMenu() {
+    this.showMobileMenu = !this.showMobileMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: Event) {
+    const clickedInside = this.elementRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.showModal) {
+      this.showModal = false;
+    }
+  }
 }

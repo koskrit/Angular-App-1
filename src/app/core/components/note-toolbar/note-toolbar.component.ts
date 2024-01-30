4b85033337438d748672b935451aae2d3f998d67
koskrit <koskritm@gmail.com>
2024-01-30 09:56:25 +0200

    Add Conditionality to Toolbar Buttons Render

--- src/app/core/components/note-toolbar/note-toolbar.component.ts
+++ src/app/core/components/note-toolbar/note-toolbar.component.ts
@@ -11,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class NoteToolbarComponent {
   @Input() note: Note | undefined;
+  @Input() isEditor: boolean = false;
 
   noteUrl: string | undefined;
 

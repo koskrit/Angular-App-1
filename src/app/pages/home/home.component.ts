b8d70586aa2ffae179d62e42c6c2061f7e79018b
koskrit <koskritm@gmail.com>
2024-02-02 18:49:34 +0200

    Minor Styling Changes & Conditional Render on Home Hero

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,18 @@
+import { AuthService } from './../../services/auth.service';
+import { isAuthenticated } from './../../core/state/auth';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  isAuthenticated = isAuthenticated;
 
+  constructor(private AuthService: AuthService) {}
+
+  register() {
+    this.AuthService.login();
+  }
 }

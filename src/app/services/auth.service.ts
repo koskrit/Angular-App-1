27f3c3d4ffa8242164342950dfccc927fa9308af
koskrit <koskritm@gmail.com>
2024-02-02 10:33:23 +0200

    Display Loader on Auth Start

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,24 +1,31 @@
 import { Injectable } from '@angular/core';
 import { OidcSecurityService } from 'angular-auth-oidc-client';
 import { environment } from '../../environments/environment.development';
+import { NotificationService } from './notification.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private oidcSecurityService: OidcSecurityService) {}
+  constructor(
+    private oidcSecurityService: OidcSecurityService,
+    private toast: NotificationService
+  ) {}
 
   login() {
+    this.toast.loader(true);
     this.oidcSecurityService.authorize();
   }
 
   logout() {
+    this.toast.loader(true);
     this.oidcSecurityService
       .logoff()
       .subscribe((result) => console.log(result));
   }
 
   register() {
+    this.toast.loader(true);
     const callbackUrl = environment.urls.registerCallback;
     const params = new URLSearchParams(`redirectUrl=${callbackUrl}`);
     window.location.href = `${environment.urls.register}?` + params.toString();
